1e3f6fc9a45ad6f78acce399a6f922f2b45b3e5d
Carlos Eduardo Papacidero <carlos.papacidero@venturus.org.br>
2017-09-02 13:05:04 -0300

    Adding some details

--- dist/scripts/CaseStudy.jsx
+++ dist/scripts/CaseStudy.jsx
@@ -119,7 +119,8 @@ class CaseStudy extends React.Component {
                         <InputText class="name form-control" type="name" callback={this.handleChanges.bind(this)} placeholder="Name" value={this.state.name}/>
                     </div>
 
-                    <button onClick={this.previousStep.bind(this)} className="btn btn-primary">Previous</button> <button onClick={this.nextStep.bind(this)}  className="btn btn-primary">Next</button>
+                    <button onClick={this.previousStep.bind(this)} className="btn btn-primary">Previous</button>
+                    <button onClick={this.nextStep.bind(this)}  className="btn btn-primary">Next</button>
                 </div>
 
                 <div style={{display: this.state.step == 3 ? 'block' : 'none' }}>
@@ -129,7 +130,8 @@ class CaseStudy extends React.Component {
                         <InputText class="email form-control" type="email" callback={this.handleChanges.bind(this)} placeholder="E-mail" value={this.state.email}/>
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
-                    <button  onClick={this.previousStep.bind(this)} className="btn btn-primary">Previous</button> <button  onClick={this.checkAndSubmit.bind(this)} className="btn btn-primary">Submit</button>
+                    <button  onClick={this.previousStep.bind(this)} className="btn btn-primary">Previous</button>
+                    <button  onClick={this.checkAndSubmit.bind(this)} className="btn btn-success ">Submit</button>
                 </div>
 
 
